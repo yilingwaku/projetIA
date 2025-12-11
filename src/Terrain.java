d4d1bc27f5044610c3f9242225058a7f2376fd96
Matis Basso <91749838+Exyo02@users.noreply.github.com>
2025-12-11 16:39:27 +0100

    Initialisation du terrain

---
+++ src/Terrain.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Terrain {
+    private final int width=30,height=30;
+    private final int[][] terrain=new int[width][height];
+
+    enum CaseType{
+        EMPTY,
+        ANOMALIE,
+        BASE
+    }
+
+    public Terrain(){
+        initialize();
+    }
+
+    private void initialize(){
+        Random random = new Random();
+
+        for (int x=0;x<width;x++){
+            for (int y=0;y<height;y++){
+                terrain[x][y] = CaseType.EMPTY.ordinal();
+            }
+        }
+        terrain[width/2][height/2] = CaseType.BASE.ordinal();
+
+        for (int nbA = 0;nbA<10;nbA++){
+            terrain[random.nextInt(width)][random.nextInt(height)]=CaseType.ANOMALIE.ordinal();
+        }
+    }
+
+    public void print(){
+        for (int x=0;x<width;x++){
+            for (int y=0;y<height;y++){
+                System.out.printf(
+                        terrain[x][y]==0?".":
+                        terrain[x][y]==1?"#":"|");
+                System.out.printf("\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public getCase(int x,int y){
+        return terrain[x][y];
+    }
+
+}
---
+++ src/main2.java
@@ -0,0 +1,6 @@
+public class main2 {
+    public static void main(String[] args) {
+        Terrain terrain = new Terrain();
+        terrain.print();
+    }
+}
